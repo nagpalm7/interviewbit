27da1e36b5bb26d7b40b6906a63dcd85c81c606f
Mohit Nagpal <nagpalm7@gmail.com>
2020-08-31 14:51:53 +0530

    add axios

--- frontend/src/pages/interview/Interview.js
+++ frontend/src/pages/interview/Interview.js
@@ -1,12 +1,52 @@
 import React from "react";
-import CustomTable from "../../components/Table/Table";
 
-const Interviewer = () => {
-  return (
-    <div>
-      <CustomTable />
-    </div>
-  );
-};
+class CustomTable extends React.Component {
+  constructor(props) {
+    this.state = {
+      data: [],
+      columns: [],
+    };
+  }
+  render() {
+    return (
+      <div className="site-card-border-less-wrapper">
+        <CustomTable />
+      </div>
+    );
+  }
+}
 
-export default Interviewer;
+export default CustomTable;
+
+const dataSource = [
+  {
+    key: "1",
+    name: "Mike",
+    age: 32,
+    address: "10 Downing Street",
+  },
+  {
+    key: "2",
+    name: "John",
+    age: 42,
+    address: "10 Downing Street",
+  },
+];
+
+const columns = [
+  {
+    title: "Name",
+    dataIndex: "name",
+    key: "name",
+  },
+  {
+    title: "Age",
+    dataIndex: "age",
+    key: "age",
+  },
+  {
+    title: "Address",
+    dataIndex: "address",
+    key: "address",
+  },
+];
